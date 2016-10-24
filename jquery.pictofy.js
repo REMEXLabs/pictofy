456964cec71d720132facd9c32782d42cf39156f
AlexVonB <AlexVonB@users.noreply.github.com>
2016-10-24 16:21:51 +0200

    preload all pictos' uris and mark pictofied words

--- jquery.pictofy.js
+++ jquery.pictofy.js
@@ -137,60 +137,58 @@
 
     var $res = $.res(opts.resUrl);
 
-    $('.' + opts.class).tooltipster({
-      updateAnimation: null,
-      trigger: 'click',
-      theme: 'tooltipster-picto',
-      onlyOne: true,
-      interactive: true,
-      content: opts.preContent,
-      functionReady: function(origin) {
+    $('.' + opts.class).each(function(idx, el) {
+      // Remove and spaces
+      var word = el.innerHTML.toLowerCase().trim();
+      var lang = (typeof AS_picsupport === undefined) ? opts.lang :
+          AS_picsupport.language || opts.lang;
 
-        if (origin.attr('data-picto-cached') == 'cached') {
-          return true;
-        }
-
-        // Remove and spaces
-        var word = origin[0].innerHTML.toLowerCase().trim();
-        var lang = (typeof AS_picsupport === undefined) ? opts.lang :
-            AS_picsupport.language || opts.lang;
-
-        var $container = $('<div>')
-            .addClass('picto-container');
-
-        $res.queries().addQuery().addProp('http://openurc.org/ns/res#type', 
-            'http://openurc.org/restypes#pictogram')
-          .addPropWithDescs('http://purl.org/dc/elements/1.1/title', word)
-            .addDesc('lang', lang)
-            .complete()
+      $res.queries().addQuery().addProp('http://openurc.org/ns/res#type', 
+          'http://openurc.org/restypes#pictogram')
+        .addPropWithDescs('http://purl.org/dc/elements/1.1/title', word)
+          .addDesc('lang', lang)
           .complete()
-          .send()
-          .done(function(responses){
-            if (responses.firstResponse().numberOfResources() > 0) {
-              var src = responses.getVeryFirstGlobalAt();
-              src = src.replace('http://', '//');
-              var name = responses.firstResponse()
-                  .firstResource()
-                  .props
-                  .find(function(prop){
-                    return prop.name === 'http://openurc.org/ns/res#name';
-                  })
-                  .value;
-              $container.append($('<img>').attr('src', src));
-              origin.tooltipster('content', $container);
-              appendStars(name, origin, opts);
-            } else {
-              origin.tooltipster('content', opts.failContent);
-            }
-            origin.attr('data-picto-cached', 'cached');
-          })
-          .fail(function(response){
-            origin.tooltipster('content', opts.failContent);
-            console.log(response.error);
-            console.log(response.status);
-          });
-        return true;
-      }
+        .complete()
+        .send()
+        .done(function(responses){
+          if (responses.firstResponse().numberOfResources() > 0) {
+            var src = responses.getVeryFirstGlobalAt();
+            src = src.replace('http://', '//');
+            var name = responses.firstResponse()
+              .firstResource()
+              .props
+              .find(function(prop){
+                return prop.name === 'http://openurc.org/ns/res#name';
+              })
+              .value;
+            $(el).attr('data-picto-src', src);
+            $(el).attr('data-picto-name', name);
+
+            $(el).tooltipster({
+              updateAnimation: null,
+              trigger: 'click',
+              theme: 'tooltipster-picto',
+              onlyOne: true,
+              interactive: true,
+              content: opts.preContent,
+              functionReady: function(origin) {
+
+                var $container = $('<div>')
+                    .addClass('picto-container');
+
+                $container.append($('<img>').attr('src', src));
+                origin.tooltipster('content', $container);
+                appendStars(name, origin, opts);
+
+                return true;
+              }
+            });
+          }
+        })
+        .fail(function(response){
+          console.log(response.error);
+          console.log(response.status);
+        });
     });
 
     return this;
